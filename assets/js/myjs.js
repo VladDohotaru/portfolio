c5d43fd08d217dfc39a94727778e700bbeb33c45
vlad <dohotaruv@gmail.com>
2018-06-03 20:20:38 +0300

    up

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -1,21 +1,21 @@
-var elem = $( '#myModal' )[0];
 
-
-$( document ).on( 'keydown', function ( e ) {
-    if ( e.keyCode === 27 ) { // ESC
-        $( elem ).hide();
-    }
-});
-
-$(".close").on('click', function() {
-    this.parentElement.style.display = 'none';
-})
-
-$('.portfolio-detail > i > img').on('click', function() {
-    this.parentElement.parentElement.getElementsByClassName("modal-content")[0].src = this.src;
-    // $('# #img01').attr('src', this.src);
-    this.parentElement.parentElement.getElementsByClassName("modal")[0].style.display = 'block';
-    // $('#modal_test #img01').css('display', 'block');
-    // alert($('#myModal').src);
+$(document).ready(function() {
+    $(".close").on('click', function() {
+        this.parentElement.style.display = 'none';
+    })
     
+    $('.portfolio-detail > i > img').on('click', function() {
+        this.parentElement.parentElement.getElementsByClassName("modal-content")[0].src = this.src;
+        this.parentElement.parentElement.getElementsByClassName("modal")[0].style.display = 'block';
+        this.parentElement.parentElement.getElementsByClassName("modal")[0].children[2].innerHTML = this.alt;
+    });
+
+    
+    $(document).keydown(function(e) {
+        // ESCAPE key pressed
+        if (e.keyCode == 27) {
+            open(location, '_self').close();
+        }
+    });
 })
+
