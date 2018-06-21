e6bdb0750a685a0d7460357b27c7cae2986b9323
vldo <vldo@netop.com>
2018-06-21 18:53:15 +0300

    up

--- assets/external/contact.php
+++ assets/external/contact.php
@@ -4,7 +4,7 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
-$to = 'your_email_address';
+$to = 'ionburungiu@gmai.com';
 $subject = 'Message From Your Website Contact Form';
 
 $body = "";
